f54965e97ac74b1794018d01947ceaf2f28b2a4a
Polawat Phetra <pphetra@gmail.com>
2023-07-25 19:30:13 +0700

    using watermill gochannel implementation

--- go.mod
+++ go.mod
@@ -5,6 +5,14 @@ go 1.20
 require github.com/stretchr/testify v1.8.4
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
+
+require (
+	github.com/ThreeDotsLabs/watermill v1.3.2
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/lib/pq v1.10.9
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- internal/infra/event_bus/channel_event_bus.go
+++ internal/infra/event_bus/channel_event_bus.go
@@ -2,15 +2,37 @@ package infra_event_bus
 
 import (
 	shared_domain "taejai/internal/shared/domain"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
 )
 
 type ChannelEventBus struct {
+	pubSub *gochannel.GoChannel
 }
 
 func NewChannelEventBus() *ChannelEventBus {
-	return &ChannelEventBus{}
+	pubSub := gochannel.NewGoChannel(
+		gochannel.Config{},
+		watermill.NewStdLogger(false, false),
+	)
+	return &ChannelEventBus{
+		pubSub,
+	}
 }
 
 func (b *ChannelEventBus) Publish(event shared_domain.DomainEvent) error {
-	return nil
+	payload, err := event.GetPayload()
+	if err != nil {
+		return err
+	}
+	msg := message.NewMessage(
+		event.GetAggregateId(),
+		payload,
+	)
+	msg.Metadata.Set("aggregate_name", event.GetAggregateName())
+	msg.Metadata.Set("aggregate_id", event.GetAggregateId())
+
+	return b.pubSub.Publish(event.GetEventName(), msg)
 }
